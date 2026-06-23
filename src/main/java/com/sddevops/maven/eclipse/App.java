e723220ec1261b3398ac7fab137421bc11db985a
qyazid-project <qyazid@gmail.com>
2026-06-23 20:52:58 +0800

    (feat) Add test line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -5,6 +5,7 @@ package com.sddevops.maven.eclipse;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Ruddybehappy!");
+        System.out.println("Have a nice day!");
     }
 }
