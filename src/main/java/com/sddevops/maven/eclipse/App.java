ebaaa7e6290c156e091f218b502f5a7d828e4070
qyazid-project <qyazid@gmail.com>
2026-06-23 22:38:36 +0800

    second commit

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Ruddybehappy!");
         System.out.println("Have a good one!");
+        System.out.println("New Line");
     }
 }
